6660376e67f0818f5d9cb2295bf7bbedddb2924c
emilhaegglund <haegglund.emil@gmail.com>
2016-04-12 10:08:00 +0200

    Changed name to schavott

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-setup(name='staellning',
+setup(name='schavott',
       version='0.1',
       description='Scaffolding in real-time',
       url='http://github.com/emilhaegglund/staellning',
@@ -9,6 +9,5 @@ setup(name='staellning',
       license='',
       install_requires=[
         'bokeh',
-        'watchdog'],
-      scripts=['bin/staellning']
+        'watchdog']
       )
